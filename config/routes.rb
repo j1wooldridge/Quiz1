68bee3fe231b40dc568317885fff6afbb051be79
Jacques Wooldridge <jacques.wooldridge@gmail.com>
2019-11-08 17:47:44 +0000

    Add updated source changed up until 2019-11-08 11:50

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
-    root 'static#show'          
+    #root 'static#show'
+    root 'books#index'          
 end
