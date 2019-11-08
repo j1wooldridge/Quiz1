510d84d08b5c38d8d81ccd01d14ff37acf41f04e
Jacques Wooldridge <jacques.wooldridge@gmail.com>
2019-11-08 23:02:08 +0000

    Add updated source changed up until 2019-11-08 17:10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,10 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    #@books = Book.all
+    @books = Book.order("name").page(params[:page]).per(3)
+  end
+
+  def new 
+        @books = Book.new              
   end
 end
