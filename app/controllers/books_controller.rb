3569071f44be49898953754931bbb545c760864a
Jacques Wooldridge <jacques.wooldridge@gmail.com>
2019-11-11 21:35:37 +0000

    Add updated source changed up until 2019-11-11 15:40

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,26 @@ class BooksController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+     @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update_attributes(book_params)
+    redirect_to root_path
+  end
+
+  def destroy
+  @book = Book.find(params[:id])
+  @book.destroy
+  redirect_to root_path
+end
+
   private
 
   def book_params
